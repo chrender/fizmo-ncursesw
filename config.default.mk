c5ed46162a61df2f56388c33089f47dc0e2c1b19
chrender <chrender>
2010-10-01 06:21:02 +0000

    - Cleaned up .mk files.

--- config.default.mk
+++ config.default.mk
@@ -5,12 +5,12 @@ CC = gcc
 AR = ar
 CFLAGS = -Wall -Wextra
 
+ifneq ($(DESTDIR),)
+INSTALL_PREFIX = $(DESTDIR)
+else
+INSTALL_PREFIX = /usr/local
+endif
 INSTALL_PREFIX = /Users/chrender/opt/fizmo
-#ifneq ($(DESTDIR),)
-#INSTALL_PREFIX = $(DESTDIR)
-#else
-#INSTALL_PREFIX = /usr/local
-#endif
 
 # If defined, install goes to "$(INSTALL_PREFIX)/($FIZMO_BIN_DIR)" instead of
 # "(INSTALL_PREFIX)/games" (usually use to subsitute "bin" for "games").
