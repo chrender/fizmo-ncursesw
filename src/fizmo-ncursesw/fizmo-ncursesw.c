e0ee2d6faebdc178608c48eb1fa1b3062e91bdc8
chrender <chrender>
2011-09-07 19:49:07 +0000

    - Updated fizmo-ncursesw changelog and version numbers for first release    candidate.

--- src/fizmo-ncursesw/fizmo-ncursesw.c
+++ src/fizmo-ncursesw/fizmo-ncursesw.c
@@ -68,7 +68,7 @@
 
 #include "../locales/fizmo_ncursesw_locales.h"
 
-#define FIZMO_NCURSESW_VERSION "0.7.0-b11"
+#define FIZMO_NCURSESW_VERSION "0.7.0"
 
 #ifdef ENABLE_X11_IMAGES
 #include <drilbo/drilbo.h>
